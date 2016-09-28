e07d4fc75e8c2d83b6088197354c190bdd6ff324
visraman26 <visraman26@gmail.com>
2016-09-29 01:03:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ var img=document.getElementById('madi');
 var marginLeft=0;
 function moveRight()
 {
-    marginLeft= marginLeft + 10px;
+    marginLeft= marginLeft + 10;
     img.style.marginLeft= marginLeft +'px';
     
 }
