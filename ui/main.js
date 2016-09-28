ee90a330312a9e5d004bf610bd14c5170e267081
visraman26 <visraman26@gmail.com>
2016-09-29 00:53:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,9 @@ console.log('Loaded!');
 
 var element=document.getElementById('main-text');
 element.innerHTML= "new value";
+
+var img=document.getElementById('imad');
+img.onclick=function(){
+ img.style.marginLeft='100px';   
+    
+};
