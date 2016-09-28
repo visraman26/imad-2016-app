1b93e22023da098b1b0e9692c495c18bbf381548
visraman26 <visraman26@gmail.com>
2016-09-28 23:58:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ app.get('/article-one', function (req, res) {
    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two', function (req, res) {
-  res.send('article two will be served here');
+   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 app.get('/article-three', function (req, res) {
-  res.send('article three will be served here');
+  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 
