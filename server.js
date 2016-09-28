8af09f62169c02e70ebd2ff8b546841524430071
visraman26 <visraman26@gmail.com>
2016-09-28 23:33:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,16 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/article-one', function (req, res) {
+  res.send('article one will be served here');
+});
+app.get('/article-two', function (req, res) {
+  res.send('article two will be served here');
+});
+app.get('/article-three', function (req, res) {
+  res.send('article three will be served here');
+});
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
