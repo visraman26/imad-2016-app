c58e37f21af641c24554a39817560475f296fea6
visraman26 <visraman26@gmail.com>
2016-09-29 09:25:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,6 +19,22 @@ app.get('/article-three', function (req, res) {
 });
 
 
+
+var counter=0;
+app.get('/counter', function (req, res) {
+    counter=counter+1;
+  res.send(counter.toString());
+});
+
+
+
+
+
+
+
+
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
