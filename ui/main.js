6186660d5707a20cbb824e76936d7c05c1d688b2
visraman26 <visraman26@gmail.com>
2016-09-29 09:33:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,10 @@
 console.log('Loaded!');
 
-var element=document.getElementById('main-text');
-element.innerHTML= "new value";
-
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveRight()
+var button=document.getElementById('counter');
+var counter=0;
+button.onclick=function()
 {
-    marginLeft= marginLeft + 1;
-    img.style.marginLeft= marginLeft +'px';
-    
+    counter=counter+1;
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
 }
-img.onclick=function(){
-    var interval=setInterval(moveRight,50);
-  
-    
-};
