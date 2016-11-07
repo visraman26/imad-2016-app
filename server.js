b599bba608566b9dad4a1e4826c915c564bbcf8b
visraman26 <visraman26@gmail.com>
2016-11-07 15:54:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/images/my1.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'my1.jpg'));
 });
 
 
