09c597be1395959f9637c4283a6bc9b1b1ce5907
visraman26 <visraman26@gmail.com>
2016-11-07 15:42:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/', function (req, res) {
 
 
 
-app.get('/', function (req, res) {
+app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
