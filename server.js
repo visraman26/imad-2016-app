03d2111d6c5c286849fa8faec9689dd4a03af876
visraman26 <visraman26@gmail.com>
2016-11-07 15:46:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
@@ -12,10 +17,6 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/images/my1.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'my1.jpg'));
 });
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
 
 
 
