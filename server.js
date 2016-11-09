be29c588d40e34671bb890155d8edcc9ef277a8b
visraman26 <visraman26@gmail.com>
2016-11-09 20:35:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.get('/test', function (req, res) {
             res.status(500).send(err.toString());
             
         }else{
-            res.send(JSON.stringify(result));
+            res.send(JSON.stringify(result.rows));
         }
         
     });
